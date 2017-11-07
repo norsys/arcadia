6753dbc0a2712c3347ce78ece8d1b1633dc57393
Yan MATAGNE <ymatagne@norsys.fr>
2017-11-07 15:53:18 -0500

    feat(resume) : Add resume page

--- src/app/components/game/planets/planets.component.ts
+++ src/app/components/game/planets/planets.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { QuestionsService } from '../../../services/questions.service'
-import { Question, Position } from '../../../models'
+import { ResponseService } from '../../../services/response.service'
+
+import { Question, Position, Response } from '../../../models'
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
@@ -10,23 +12,19 @@ import 'rxjs/add/operator/switchMap';
   selector: 'app-planets',
   templateUrl: './planets.component.html',
   styleUrls: ['./planets.component.scss'],
-  providers: [QuestionsService]
+  providers: [QuestionsService, ResponseService]
 })
 export class PlanetsComponent implements OnInit {
 
   public categoryId: String;
   public questions: Array<Question>;
   public positions: Array<Position> = new Array();
-
-  constructor(private route: ActivatedRoute, private router: Router, private questionsService: QuestionsService) {
-    this.route.params.subscribe(params => {
-      this.categoryId = params['categoryId'];
-      this.questionsService.getAll().then((response) => {
-        this.questions = response.json().filter(question => question.category_id.toString() === this.categoryId)
-        this.writeAlienPosition();
-
-      });
-    });
+  private responses: Array<Response>;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private questionsService: QuestionsService,
+    private responsesService: ResponseService) {   
   }
 
   writeAlienPosition() {
@@ -49,15 +47,32 @@ export class PlanetsComponent implements OnInit {
     return "url('/assets/img/planets/zoom/surface-planet-" + this.categoryId + ".png')";
   }
 
-  getAlien(question) {
-    return '/assets/img/planets/zoom/aliens/planet-' + this.categoryId + '-alien-' + ++question + '.png';
+  getAlien(index) {
+    if (this.responses.filter(response => response.question_id == this.questions[index].id).length > 0) {
+      return '/assets/img/rum.png'
+    } else {
+      return '/assets/img/planets/zoom/aliens/planet-' + this.categoryId + '-alien-' + ++index + '.png';
+    }
   }
+
   openDefi(id) {
     this.router.navigate(['/questions', this.questions[id].id]);
   }
 
-  ngOnInit() {
+  isUp() {
+    return this.questions && this.responses;
+  }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.categoryId = params['categoryId'];
+      this.questionsService.getAll().then((response) => {
+        this.questions = response.json().filter(question => question.category_id.toString() === this.categoryId)
+        this.responsesService.getAllResponseByUser().then(response => this.responses = response.json());
+        this.writeAlienPosition();
+
+      });
+    });
   }
 
 }
