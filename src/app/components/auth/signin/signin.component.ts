6d53547350d662c537fc99085666f0aebf42f18e
med amine <maammach@norsys.fr>
2018-01-24 17:52:35 +0000

    fix: login/Email proposer une minuscule en tant que 1ère lettre plutôt qu’une majuscule

--- src/app/components/auth/signin/signin.component.ts
+++ src/app/components/auth/signin/signin.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Alien } from '../../../models/index';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import {DisplayService} from '../../../services/display.service';
 
 @Component({
   selector: 'app-signin',
@@ -17,19 +18,32 @@ export class SigninComponent {
   @Output() onSwipe = new EventEmitter<boolean>();
 
   constructor(private auth: AuthService,
-    private router: Router) { }
+              private displayService: DisplayService,
+              private router: Router) { }
 
-    /*DOM events*/
+  /*DOM events*/
   onSubmit() {
+    this.alien.email = this.alien.email.toLowerCase();
     this.auth.login(this.alien)
       .then((alien) => {
         this.auth.setAlienInLocalStorage(alien.json().user);
+        this.displayService.setShowHeader(true);
         this.router.navigate(['/home']);
         this.error = 'Hello '.concat(alien.json().nickname).concat(' !');
       })
       .catch((err) => {
-        this.error = 'Problem during authentication. Check your connection';
+        this.handleErrorLogin(err);
       });
+  }
+
+  private handleErrorLogin(err) {
+    const errorCode = err.json()[0].errorCode;
+    if (errorCode === 'Unauthorized') {
+      this.error = 'Les données que vous avez entré sont incorrects';
+      setTimeout(() => {this.error = ''}, 2000);
+    } else {
+      this.error = 'Problem during authentication. Check your connection';
+    }
   }
 
   displayLogin() {
