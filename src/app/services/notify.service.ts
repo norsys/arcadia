8dfe54d906b58a52903eea0e8386161c28c36c00
Zakaryae Azrour AMEKKAOUI <zakaryae.amekkaoui@gmail.com>
2018-03-06 15:25:49 +0000

    N° 5846 - AMÉLIORATION : Ajouter un boss de fin !

--- src/app/services/notify.service.ts
+++ src/app/services/notify.service.ts
@@ -1,22 +1,48 @@
 import { Injectable } from '@angular/core';
 import {NotificationsService} from 'angular2-notifications';
+import {PercentageService} from './percentage.service';
+import {Alien} from '../models';
 
 @Injectable()
 export class NotifyService {
 
-  constructor(private notif: NotificationsService) { }
+  percentage: Number;
+  percent: Boolean;
+  constructor(private notif: NotificationsService,
+              private percentageService: PercentageService) { }
 
-  notifyWithSuccess() {
-    this.notif.success(
-      'Success',
-      'Votre réponse est enregistrée avec succès',
-      {
-        timeOut: 2000,
-        showProgressBar: false,
-        pauseOnHover: false,
-        clickToClose: false,
-        maxLength: 50
-      }
-    );
+ verifierPourcentage() {
+      this.percentageService.calculatePercentage();
+      this.percentageService.observable.subscribe(percentageResult => {
+        this.percentage = percentageResult;
+        if (this.percentage === 100) {
+          this.percent = true;
+        }
+      });
+      if (this.percent) {
+        this.notif.info(
+          'Wonderful',
+          'Tu as repondu à toutes les questions',
+          {
+            timeOut: 3000,
+            showProgressBar: false,
+            pauseOnHover: false,
+            clickToClose: false,
+            maxLength: 100
+          }
+        );
+      }else {
+      this.notif.success(
+        'Bravo',
+        'Tu as réussi le défis !',
+        {
+          timeOut: 2000,
+          showProgressBar: false,
+          pauseOnHover: false,
+          clickToClose: false,
+          maxLength: 50
+        }
+      );
+    }
   }
 }
